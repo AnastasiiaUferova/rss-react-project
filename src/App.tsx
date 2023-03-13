c200a7fe526ae89429cda74197d1ec669a84ba2d
‘Anastasiia <miss.iuferova@yandex.ru>
2023-03-13 17:13:33 +0100

    feat: add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import AboutUs from './components/AboutUs';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
-    return <div>New React App</div>;
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about-us" element={<AboutUs />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
+    );
   }
 }
