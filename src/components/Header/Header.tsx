0bcc42ec4879986246a366b1d9b52556f555c202
‘Anastasiia <miss.iuferova@yandex.ru>
2023-03-14 17:35:21 +0100

    fix: fix pathname logic in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,9 +6,16 @@ import { WithRouterProps, withRouter } from '../../utils/withRouter';
 class Header extends Component<WithRouterProps> {
   render() {
     const { location } = this.props;
+
+    type StringObject = { [key: string]: string };
+
+    const pathNames: StringObject = {
+      '/about-us': 'About Us',
+      '/': 'Home',
+    };
     return (
       <div className="header">
-        <p>{location.pathname}</p>
+        <p>{pathNames[location.pathname] || 'Not Found'}</p>
         <div className="header__navlinks">
           <NavLink to="/">Home</NavLink>
           <NavLink to="about-us">About Us</NavLink>
