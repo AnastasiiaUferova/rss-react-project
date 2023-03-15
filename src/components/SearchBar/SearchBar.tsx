3f9e8206b745f86a04d6a2ccf6d0649b70325612
‘Anastasiia <miss.iuferova@yandex.ru>
2023-03-15 13:31:22 +0100

    feat: add input functionality

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
 
-export default class SearchBar extends Component {
+type State = {
+  input: string;
+};
+
+export default class SearchBar extends Component<State> {
+  state = { input: '' };
+
+  handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    const inputValue = e.currentTarget.value;
+    localStorage.setItem('input', inputValue);
+    this.setState({ input: inputValue });
+  };
+
+  componentDidMount() {
+    const inputFromLocalStorage = localStorage.getItem('input') || '';
+    this.setState({ input: inputFromLocalStorage });
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('input', this.state.input);
+  }
+
   render() {
     return (
       <div className="search">
         <div className="search-container">
           <form className="search__form">
-            <input className="search__form__input" type="text"></input>
+            <input
+              className="search__form__input"
+              type="text"
+              value={this.state.input}
+              onChange={this.handleChange}
+            ></input>
             <button className="search__form__button" type="submit"></button>
           </form>
         </div>
+        <p>Input from LocalStorage:{localStorage.getItem('input')}</p>
       </div>
     );
   }
--- src/utils/withRouter.tsx
+++ src/utils/withRouter.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export interface WithRouterProps {
   location: ReturnType<typeof useLocation>;
-  params: Record<string, string>;
-  navigate: ReturnType<typeof useNavigate>;
 }
 
 export const withRouter = <Props extends WithRouterProps>(
@@ -12,14 +10,10 @@ export const withRouter = <Props extends WithRouterProps>(
 ) => {
   return (props: Omit<Props, keyof WithRouterProps>) => {
     const locationHook = useLocation();
-    const paramsHook = useParams();
-    const navigateHook = useNavigate();
 
     const propsWithRouter: Props = {
       ...(props as Props),
       location: locationHook,
-      params: paramsHook,
-      navigate: navigateHook,
     };
 
     return <WrappedComponent {...propsWithRouter} />;
