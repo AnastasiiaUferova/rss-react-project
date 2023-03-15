e09df0bb13afecfc855dfc0474da02971dd224dc
‘Anastasiia <miss.iuferova@yandex.ru>
2023-03-15 12:05:41 +0100

    feat: add notFound component styles

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import './NotFound.css';
+import NotFoundPic from '../../images/error.png';
 
 export default class NotFound extends Component {
   render() {
-    return <div>Not Found</div>;
+    return (
+      <div className="not-found">
+        <img src={NotFoundPic} alt="not-found picture" className="not-found__pic"></img>
+      </div>
+    );
   }
 }
